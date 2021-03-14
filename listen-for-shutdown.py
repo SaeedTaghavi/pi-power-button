e295986cacf20278b5b71ac1b27e347402098e7b
saeed <saeed.taghavi.v@gmail.com>
2021-03-14 15:45:47 +0330

    modify listen-for-shutdown.py; now if you push button for more that two seconds the pi will shutdown

--- listen-for-shutdown.py
+++ listen-for-shutdown.py
@@ -3,10 +3,15 @@
 
 import RPi.GPIO as GPIO
 import subprocess
+from time import time
 
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(3, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.wait_for_edge(3, GPIO.FALLING)
-
-subprocess.call(['shutdown', '-h', 'now'], shell=False)
+while True:
+    GPIO.wait_for_edge(3, GPIO.FALLING)
+    t1 = time()
+    while(GPIO.input(3)==0):
+            t2=time()
+            if (t2-t1>2):
+                    subprocess.call(['shutdown', '-h', 'now'], shell=False)
